d90728bfb763b4834b3521fdd41d73d35737a1c8
mutuku85 <mutukusamuel10185@gmail.com>
2023-02-16 09:39:13 +0300

    a message

---
+++ game.py
@@ -0,0 +1,13 @@
+#this program demonstrates a guessing game
+from random import randint 
+
+#1.get user input
+user_name=input("What's your name?")
+print("Hello there"+user_name+"!")
+
+#generate a random number
+number=randint(10:50)
+#2.using a while loop
+counter=0 
+while counter<5:
+    user_number=eval(input("enter number"))

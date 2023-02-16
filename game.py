5157adde664e7ccb4d3cfe97b77baacbb42e491a
mutuku85 <mutukusamuel10185@gmail.com>
2023-02-16 10:49:08 +0300

    finalise push

--- game.py
+++ game.py
@@ -4,10 +4,23 @@ from random import randint
 #1.get user input
 user_name=input("What's your name?")
 print("Hello there"+user_name+"!")
+number=randint(10,50)
 
-#generate a random number
-number=randint(10:50)
 #2.using a while loop
 counter=0 
 while counter<5:
     user_number=eval(input("enter number"))
+    counter+=1
+    if user_number<number:
+     print("Your guess is too low")
+    elif user_number>number:
+     print("Your number is too high")
+    elif user_number==number:
+     print("You win")
+     break
+print("Game over.")
+if user_number==number:
+    print("You win")
+else:
+    print("The correct number was")
+    print(number)
